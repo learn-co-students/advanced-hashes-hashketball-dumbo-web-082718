b352fde9543a6bef085db3f55474ce1af05a3c93
Nasser Sanou <nassersanou23@gmail.com>
2018-08-20 23:25:20 +0000

    work in progress..

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,34 @@
 # Write your code here!
 
-
+def game_hash
+  
+  hash = Hash.new()
+  hash = [ :home => { 
+              :team_name => "Brooklyn Nets",
+              :colors => ["Black", "White"] 
+              :players => [ 
+                { :players_name => "Alan Anderson"}
+                { :players_name =>"Reggie Evans"  },
+                { :players_name =>"Brook Lopez"   },
+                { :players_name =>'Mason Plumlee' },
+                { :players_name =>'Jason Terry'   }
+                }
+  ]
+  
+  [  :away => { 
+            :team_name => "Charlotte Hornets"
+            :colors => ["Turquoise", "Purple"],
+            :players => [
+               {  :players_name => "Alan Anderson"  }
+               {  :players_name => 'Reggie Evans'   },
+               {  :players_name => 'Brook Lopez'    },
+               {  :players_name => 'DeSagna Diop'   }, 
+               {  :players_name => 'Ben Gordon'     },
+               {  :players_name => 'Brendan Haywood'}
+               } 
+  ]
+  
+end 
 
 
 
